12804fecb397557e607e99ca3663122c14377631
0rishav <rishavrajraj1155@gmail.com>
2024-04-23 08:58:25 +0530

    initial commit

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ connectDB();
 const app = express();
 
 // middlewares
-app.use(cors())
+app.use(cors({
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
 app.use(express.json())
 app.use(morgan("dev"))
 
